a50cf86eb87e75bd94118d5ef2c50d3560b84dca
li-rongcheng <Li.Rongcheng@gmail.com>
2022-01-28 16:46:47 +1100

    add impl and images

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
+console.log("Count-down extension loaded");
 
+let date = "08 15 2023";
+
+chrome.runtime.onInstalled.addListener(() => {
+
+    chrome.storage.sync.set({ date });
+
+    console.log("Default Date set to Aug 15, 2021");
+
+});
